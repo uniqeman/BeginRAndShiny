cafb60ea363a0c5806f4e156d3f1def46727e229
uniqeman <uniqeman@nate.com>
2014-06-08 00:03:50 +0900

    Create server.R

---
+++ server.R
@@ -0,0 +1,12 @@
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+     
+    # generate and plot an rnorm distribution with the requested
+    # number of observations
+    dist <- rnorm(input$obs)
+    hist(dist)
+    
+  })
+  
+})
